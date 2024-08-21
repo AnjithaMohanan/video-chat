6dc3e1227da3efcdb66ed44905cb75ddf5025595
Anjitha Mohanan <113471696+AnjithaMohanan@users.noreply.github.com>
2024-08-21 22:26:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const http = require("http");
-const path = require("path");
 const app = express();
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, "/frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
-});
 
 const io = require("socket.io")(server, {
   cors: {
